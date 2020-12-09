f8aef4459af13c81897c66c7664913acea0f4985
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-12-09 09:01:45 -0500

    build create and destroy action in Bulletins_controller

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -3,5 +3,18 @@ class BulletinsController < ApplicationController
     def index  
       bulletins = Bulletin.all 
       render json: bulletins
+    end 
+
+    def create 
+      bulletin = Bulletin.new(bulletin_params) 
+
+      if bulletin.save 
+        render json: bulletin 
+      end 
+    end 
+
+    def destroy 
+      bulletin = Bulletin.find(params[:id])
+      bulletin.destroy 
     end
 end
