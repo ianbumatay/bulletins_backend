b25a34baa20b8c03a1d86fec6d89e6f4a7790b9d
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-12-09 10:00:52 -0500

    build index,create,destroy action for Comment_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
-class CommentsController < ApplicationController
+class CommentsController < ApplicationController 
+
+    def index 
+      comments = Comment.all 
+      render json: CommentSerializer.new(comments)
+    end 
+
+    def create 
+      commnent = Comment.new(comment_params) 
+
+      if comment.save 
+        render json: CommentSerializer.new(comment)
+      else 
+        render json: { message: 'Comment not found' } 
+      end
+    end 
+
+    def destroy 
+      comment = Comment.find(params[:id])
+      comment.destroy 
+    end 
+
+    private 
+
+    def comment_params 
+      params.require(:comment).permit(:content) 
+    end
 end
